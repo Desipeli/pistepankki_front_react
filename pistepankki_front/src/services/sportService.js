b2a7e8a7720e5e3877130468de71fc2049c759b2
Desipeli <mr.desipeli@gmail.com>
2023-02-16 14:44:48 +0200

    order sports

--- pistepankki_front/src/services/sportService.js
+++ pistepankki_front/src/services/sportService.js
@@ -2,8 +2,19 @@ import axios from 'axios'
 
 const baseUrl = process.env.REACT_APP_BACKEND_URL + '/api/sports'
 
+const comp = (a, b) => {
+  if (a.name > b.name) {
+    return -1
+  }
+  if (a.name < b.name) {
+    return 1
+  }
+  return 0
+}
+
 const getSports = async () => {
   const res = await axios.get(baseUrl)
+  const orderedSports = res.data.sort(comp)
   return res.data
 }
 
