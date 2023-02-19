f047f537560ba9216d19e96525d33c2ec9cc3ac1
Desipeli <mr.desipeli@gmail.com>
2023-02-19 14:14:33 +0200

    change password

--- src/services/userService.js
+++ src/services/userService.js
@@ -6,4 +6,23 @@ const getAllUsers = async () => {
   return users.data.filter((u) => u.username !== 'deleted')
 }
 
+export const validatePassword = (current, p1, p2) => {
+  if (p1 !== p2) throw Error('new passwords do not match')
+  if (p1.length < 5)
+    throw Error('password must have at least 5 characters long')
+  return true
+}
+
+export const changePassword = async (current, p1, p2, user) => {
+  await axios.put(
+    baseUrl + '/changepassword',
+    { p1, p2, current },
+    {
+      headers: {
+        Authorization: user.token,
+      },
+    }
+  )
+}
+
 export default getAllUsers
