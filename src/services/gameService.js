55aae4f66a58222b3a4153e1d85248af6658b817
Desipeli <mr.desipeli@gmail.com>
2023-02-21 18:53:14 +0200

    delete match

--- src/services/gameService.js
+++ src/services/gameService.js
@@ -10,3 +10,10 @@ export const getGameById = async (id) => {
   const res = await axios.get(`${baseUrl}/${id}`)
   return res.data
 }
+
+export const deleteGame = async (id, user) => {
+  const res = await axios.delete(`${baseUrl}/${id}`, {
+    headers: { Authorization: user.token },
+  })
+  return res.data
+}
